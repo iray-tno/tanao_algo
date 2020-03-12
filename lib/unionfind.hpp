6eef338d02d950d15e658e2094b962795d612473
iray-tno <iray.tno@gmail.com>
2020-03-13 03:15:38 +0900

    Implement tests of UnionFind

--- lib/unionfind.hpp
+++ lib/unionfind.hpp
@@ -17,11 +17,25 @@ class UnionFind {
         if (data[ry] < data[rx]) std::swap(rx, ry);
         data[rx] += data[ry];
         data[ry] = rx;
-            --groups_;
+        --groups_;
         return true;
-        }
+    }
 
     bool areSame(const int x, const int y) { return getRoot(x) == getRoot(y); }
     int getRoot(const int x) { return data[x] < 0 ? x : data[x] = getRoot(data[x]); }
     int getGroupSize(const int x) { return -data[getRoot(x)]; }
 };
+
+TEST_CASE("Testing unite and areSame functions in small test") {
+    UnionFind uf(6);
+    uf.unite(0, 1);
+    uf.unite(1, 2);
+
+    uf.unite(4, 5);
+
+    CHECK(uf.areSame(0, 2));
+    CHECK(uf.areSame(4, 5));
+    CHECK(!uf.areSame(1, 4));
+    CHECK(!uf.areSame(3, 5));
+    CHECK(uf.groups == 3);
+}
--- src/entry.cpp
+++ src/entry.cpp
@@ -2,3 +2,4 @@
 #include "../deps/doctest.h"
 
 #include "../lib/factorial.hpp"
+#include "../lib/unionfind.hpp"
