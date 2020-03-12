9b6020ab43a0410a3e470ae09e5c738c1457e744
iray-tno <iray.tno@gmail.com>
2020-03-13 03:15:10 +0900

    Improve readability of UnionFind class

--- lib/unionfind.hpp
+++ lib/unionfind.hpp
@@ -2,28 +2,26 @@
 
 class UnionFind {
  private:
-    std::vector<int> p_gs_; //正なら親ノードの番号で負なら自身がルートで値は-groupsize
+    // positive: parent node id
+    // negative: -groupsize (and itself is a root node)
+    std::vector<int> data;
     int groups_;
 
  public:
-    const int nodes;
+    const int nodes; const int& groups;
+    UnionFind(const int size) : data(size, -1), groups_(size), nodes(size), groups(groups_){}
 
-    UnionFind(int size) : p_gs_(size, -1), nodes(size), groups_(size) {}
-
-    bool unite(int x, int y) {
-        int rx = root(x), ry = root(y); // XXX: 引数をそのまま使った方が高速だった（ときがあった）
-        if (rx != ry) {
+    bool unite(const int x, const int y) {
+        int rx = getRoot(x), ry = getRoot(y);
+        if (rx == ry) return false;
+        if (data[ry] < data[rx]) std::swap(rx, ry);
+        data[rx] += data[ry];
+        data[ry] = rx;
             --groups_;
-            //rx<ryにしてrxにryを連結
-            if(p_gs_[ry] < p_gs_[rx]) std::swap(rx, ry);
-            p_gs_[rx] += p_gs_[ry];
-            p_gs_[ry] = rx;
+        return true;
         }
-        return rx != ry;
-    }
 
-    bool same(int x, int y) { return root(x) == root(y); }
-    int root(int x) { return p_gs_[x] < 0 ? x : p_gs_[x] = root(p_gs_[x]); }
-    int gsize(int x) { return -p_gs_[root(x)]; }
-    int groups() const { return groups_; }
+    bool areSame(const int x, const int y) { return getRoot(x) == getRoot(y); }
+    int getRoot(const int x) { return data[x] < 0 ? x : data[x] = getRoot(data[x]); }
+    int getGroupSize(const int x) { return -data[getRoot(x)]; }
 };
